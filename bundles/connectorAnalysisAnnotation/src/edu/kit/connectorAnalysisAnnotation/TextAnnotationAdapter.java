620be1b3e34c14fa67d92faea5cf2ee9e4393f66
Bjarne Sauer <bjarne.sauer@student.kit.edu>
2021-01-17 15:53:10 +0100

    Create firts class structure for TextAnnotationAdapter
    - Import does not work
    - How to link between different maven modules?

--- bundles/connectorAnalysisAnnotation/src/edu/kit/connectorAnalysisAnnotation/TextAnnotationAdapter.java
+++ bundles/connectorAnalysisAnnotation/src/edu/kit/connectorAnalysisAnnotation/TextAnnotationAdapter.java
@@ -1,7 +1,14 @@
+//How to organise the import??
+import edu.kit.textannotation.annotationplugin.wizards.TextAnnotationFileWizard;
+
 public class TextAnnotationAdapter {
+	private TextAnnotationFileWizard textAnnotationFileWizard; //TextAnnotationFileWizard unkwnon
 	
 	public TextAnnotationAdapter() {
+		textAnnotationFileWizard = new TextAnnotationFileWizard();
+	}
+	
+	public void createTextAnnotationProfile(String fileName) {
 		
 	}
 }
-	
