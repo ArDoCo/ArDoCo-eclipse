6bf0b81848d6cade8ed7baf27fc464fbf8bd4fde
Bjarne Sauer <bjarne.sauer@student.kit.edu>
2021-01-18 13:03:37 +0100

    Change packaging to jar and move test class

--- bundles/connectorAnalysisAnnotation/tests/edu/kit/connectorAnalysisAnnotation/tests/TextAnnotationAdapterTest.java
+++ bundles/connectorAnalysisAnnotation/tests/edu/kit/connectorAnalysisAnnotation/tests/TextAnnotationAdapterTest.java
@@ -1,9 +1,10 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TextAnnotationAdapterTest {
 	
 	@Test
 	public void testAdapter() {
 		TextAnnotationAdapter adapter = new TextAnnotationAdapter();
+		assertTrue(False);
 	}
 }
