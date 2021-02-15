97ef47765adf97c821d5fcc162fc2c1af8ba44ff
Bjarne Sauer <bjarne.sauer@student.kit.edu>
2021-02-15 13:20:28 +0100

    Add modular test cases for creating taf and tap files

--- tests/textAnalysis.core.tests/src/textAnalysis/core/tests/DelegateTests.java
+++ tests/textAnalysis.core.tests/src/textAnalysis/core/tests/DelegateTests.java
@@ -4,17 +4,33 @@ import textAnalysis.core.Delegate;
 
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.junit.BeforeClass;
 
 public class DelegateTests {
+	private String containerName = "/ardoco-eclipse/example/annotation-example-project";
+	private String fileNameProfile = "testAnalysisAnnotationProfile01";
+	private String fileNameTaf = "testAnaylsisAnnotationTaf01";
+	private String profileName = "Requirements Document";
+	private static Delegate textAnalysisDelegate;
+	
+	@BeforeClass
+	public static void setup() {
+		textAnalysisDelegate = new Delegate();
+	}
 	
 	@Test
 	public void testLaunchWithTextAnnotation() {
-		Delegate textAnalysisDelegate = new Delegate();
-    	String containerName = "/ardoco-eclipse/example/annotation-example-project";
-    	String fileNameProfile = "testAnalysisAnnotationProfile01";
-    	String fileNameTaf = "testAnaylsisAnnotationTaf01";
-    	String profileName = "Requirements Document";
 		textAnalysisDelegate.launchWithTextAnnotation(containerName, fileNameProfile, fileNameTaf, profileName);
+	}
+	
+	@Test
+	public void testCreateTextAnnotationFile() {
+		textAnalysisDelegate.createTextAnnotationFile(containerName, fileNameTaf, profileName, "");
+	}
+	
+	@Test
+	public void testCreateTextAnnotationProfile() {
+		textAnalysisDelegate.createNewTextAnnotationProfile(containerName, fileNameProfile, profileName);
 	}
 
 }
